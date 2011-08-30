c3880660c12ea900e5f8dc6400eed13933d5a975
Ang3lFir3 <ang3lfir3@gmail.com>
2011-08-29 17:32:46 -0700

    adding v3 stuffs so I can view user repos... seems to work even tho i can't get https access over the web because of some boingo stuffs

--- telescope.rb
+++ telescope.rb
@@ -1,7 +1,25 @@
 require 'rubygems'
 require 'Httparty'
 
-result = HTTParty.get('http://github.com/api/v2/json/repos/search/' + ARGV[0])
-result["repositories"].each do |x|
- puts x["url"]
+def repos(repo)
+	result = HTTParty.get('http://github.com/api/v2/json/repos/search/' + repo)
+	result["repositories"].each do |x|
+		puts x["url"]
+	end
 end
+
+def user_repos(user)
+	result = HTTParty.get('https://api.github.com/users/' + user + '/repos')
+	result.each do |x|
+		puts x["ssh_url"] 
+		puts x["clone_url"]
+		puts '----------------------------------------------------'
+	end
+end
+
+case ARGV[0]
+when "repos" then repos ARGV[1]
+when "user_repos" then user_repos ARGV[1]
+else puts "you can't just ask a genie to be awesome"
+end
+
