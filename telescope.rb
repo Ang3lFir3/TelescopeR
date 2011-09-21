b0c77c6a1f08273d9dd0114fb1d5d37f0f7ee676
Ang3lFir3 <ang3lfir3@gmail.com>
2011-09-20 18:24:23 -0700

    adding search for gists... for some reason this keeps calling the user_repos as well :( not sure why

--- telescope.rb
+++ telescope.rb
@@ -8,16 +8,25 @@ def repos(repo)
 	end
 end
 
-def user_repos(user)
-	result = HTTParty.get('https://api.github.com/users/' + user + '/repos')
+def user_repos(repoUser)
+	pretty_print_results('https://api.github.com/users/' + repoUser + '/repos', {:ssh => "ssh_url", :clone => "clone_url"})
+end
+
+def user_gists(gistUser)
+	pretty_print_results('https://api.github.com/users/' + gistUser + '/gists', {:description => "description", :pull_url => "git_pull_url"})
+end
+
+def pretty_print_results(uri, fields)
+	result = HTTParty.get(uri)
 	result.each do |x|
-		puts "ssh: " + x["ssh_url"] 
-		puts "clone: " + x["clone_url"]
+		fields.each do |y|
+			puts y[0].to_s + x[y[1]].to_s
+		end
 		puts '----------------------------------------------------'
 	end
 end
 
-fn = {:repos => repos(ARGV[1]), :user_repos => user_repos(ARGV[1]) }
+fn = {:repos => repos(ARGV[1]), :urepos => user_repos(ARGV[1]), :ugists => user_gists(ARGV[1])}
 
 fn[ARGV[0]]
 
